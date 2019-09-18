003c6552410a9d9315d315e5cf9b80ea771101b8
anthony blanchflower <anthony.blanchflower@comparethemarket.com>
2019-09-18 11:34:22 +0100

    change owner_email

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(
     test_suite='dictlisttools.tests',
     url='https://github.com/anthonyblanchflower/dictlisttools',
     author='Anthony Blanchflower',
-    author_email='anthony.blanchflower@comparethemarket.com',
+    author_email='anthony.blanchflower@btinternet.com',
     description='Functions for manipulating a list of dictionaries'
 )
